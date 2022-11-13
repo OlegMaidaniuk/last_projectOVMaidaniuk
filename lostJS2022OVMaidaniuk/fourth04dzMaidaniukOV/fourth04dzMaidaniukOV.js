1fb0b5352f69e813a170cfe3a6e8f62e62dbef75
OlegMaidaniuk <omaydanuk@gmail.com>
2022-11-13 20:15:09 +0200

    fff kk

--- lostJS2022OVMaidaniuk/fourth04dzMaidaniukOV/fourth04dzMaidaniukOV.js
+++ lostJS2022OVMaidaniuk/fourth04dzMaidaniukOV/fourth04dzMaidaniukOV.js
@@ -1,6 +1,6 @@
 // - За допомогою циклу for і document.write() вивести 10 блоків div
 // з довільним текстом всередині
-//
+// //
 // for (let i = 0; i < 10; i++) {
 //   document.write(`<div>hello</div>`)
 // }
